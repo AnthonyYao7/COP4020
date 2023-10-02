6af6df501b988acbd8295d5d5ea2d9e34b063d26
AnthonyYao7 <anthonyjyao@gmail.com>
2023-10-01 21:56:27 -0400

    finished adding first and final sets

--- edu/ufl/cise/cop4020fa23/ExpressionParser.java
+++ edu/ufl/cise/cop4020fa23/ExpressionParser.java
@@ -46,6 +46,8 @@ import static edu.ufl.cise.cop4020fa23.Kind.CONST;
 import static edu.ufl.cise.cop4020fa23.Kind.BLOCK_OPEN;
 import static edu.ufl.cise.cop4020fa23.Kind.BLOCK_CLOSE;
 import static edu.ufl.cise.cop4020fa23.Kind.SEMI;
+import static edu.ufl.cise.cop4020fa23.Kind.BOOLEAN_LIT;
+import static edu.ufl.cise.cop4020fa23.Kind.IDENT;
 
 
 import java.lang.reflect.Array;
@@ -117,85 +119,39 @@ public class ExpressionParser implements IParser {
 	final static ArrayList<HashSet<Kind>> FOLLOW = new ArrayList<>(ASTNodeNames.values().length);
 
 	static{
-		FOLLOW.get(ASTNodeNames.Expr.ordinal()).add(RSQUARE);
-		FOLLOW.get(ASTNodeNames.Expr.ordinal()).add(RPAREN);
-
-		FOLLOW.set(ASTNodeNames.ConditionalExpr.ordinal(), FOLLOW.get(ASTNodeNames.Expr.ordinal()));
-
-		FOLLOW.set(ASTNodeNames.LogicalOrExpr.ordinal(), FOLLOW.get(ASTNodeNames.Expr.ordinal()));
-
-		FOLLOW.get(ASTNodeNames.LogicalAndExpr.ordinal()).add(BITOR);
-		FOLLOW.get(ASTNodeNames.LogicalAndExpr.ordinal()).add(OR);
-
-		FOLLOW.get(ASTNodeNames.ComparisonExpr.ordinal()).add(BITAND);
-		FOLLOW.get(ASTNodeNames.ComparisonExpr.ordinal()).add(AND);
-
-		FOLLOW.set(ASTNodeNames.PowExpr.ordinal(), FOLLOW.get(ASTNodeNames.ComparisonExpr.ordinal()));
-		FOLLOW.get(ASTNodeNames.PowExpr.ordinal()).add(EQ);
-		FOLLOW.get(ASTNodeNames.PowExpr.ordinal()).add(LE);
-		FOLLOW.get(ASTNodeNames.PowExpr.ordinal()).add(GE);
-
-		FOLLOW.set(ASTNodeNames.AdditiveExpr.ordinal(), FOLLOW.get(ASTNodeNames.PowExpr.ordinal()));
-		FOLLOW.get(ASTNodeNames.AdditiveExpr.ordinal()).add(PLUS);
-		FOLLOW.get(ASTNodeNames.AdditiveExpr.ordinal()).add(MINUS);
-
-		FOLLOW.set(ASTNodeNames.MultiplicativeExpr.ordinal(), FOLLOW.get(ASTNodeNames.AdditiveExpr.ordinal()));
-		FOLLOW.get(ASTNodeNames.MultiplicativeExpr.ordinal()).add(TIMES);
-		FOLLOW.get(ASTNodeNames.MultiplicativeExpr.ordinal()).add(DIV);
-		FOLLOW.get(ASTNodeNames.MultiplicativeExpr.ordinal()).add(MOD);
-
-		FOLLOW.set(ASTNodeNames.UnaryExpr.ordinal(), FOLLOW.get(ASTNodeNames.MultiplicativeExpr.ordinal()));
-		FOLLOW.get(ASTNodeNames.UnaryExpr.ordinal()).add(EXP);
-
-		FOLLOW.set(ASTNodeNames.PostfixExpr.ordinal(), FOLLOW.get(ASTNodeNames.UnaryExpr.ordinal()));
-
-		FOLLOW.set(ASTNodeNames.PrimaryExpr.ordinal(), new HashSet<>(Arrays.asList(RARROW, COMMA, SEMI)));
-
-		FOLLOW.set(ASTNodeNames.PixelSelector.ordinal(), new HashSet<>(Arrays.asList(BLOCK_CLOSE, COMMA, SEMI)));
-
-		FOLLOW.set(ASTNodeNames.ChannelSelector.ordinal(), new HashSet<>(Arrays.asList(PLUS, MINUS, TIMES, EXP, DIV, MOD)));
-
-		FOLLOW.set(ASTNodeNames.ExpandedPixelExpr.ordinal(), FOLLOW.get(ASTNodeNames.PrimaryExpr.ordinal()));
-
+		FOLLOW.set(ASTNodeNames.Expr.ordinal(), new HashSet<>(Arrays.asList(COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.ConditionalExpr.ordinal(), new HashSet<>(Arrays.asList(COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.LogicalOrExpr.ordinal(), new HashSet<>(Arrays.asList(COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.LogicalAndExpr.ordinal(), new HashSet<>(Arrays.asList(BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.ComparisonExpr.ordinal(), new HashSet<>(Arrays.asList(BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.PowExpr.ordinal(), new HashSet<>(Arrays.asList(LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.AdditiveExpr.ordinal(), new HashSet<>(Arrays.asList(EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.MultiplicativeExpr.ordinal(), new HashSet<>(Arrays.asList(PLUS, MINUS, EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.UnaryExpr.ordinal(), new HashSet<>(Arrays.asList(TIMES, DIV, MOD, PLUS, MINUS, EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.PostfixExpr.ordinal(), new HashSet<>(Arrays.asList(TIMES, DIV, MOD, PLUS, MINUS, EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.PrimaryExpr.ordinal(), new HashSet<>(Arrays.asList(TIMES, DIV, MOD, PLUS, MINUS, EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.ChannelSelector.ordinal(), new HashSet<>(Arrays.asList(TIMES, DIV, MOD, PLUS, MINUS, EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.PixelSelector.ordinal(), new HashSet<>(Arrays.asList(TIMES, DIV, MOD, PLUS, MINUS, EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
+		FOLLOW.set(ASTNodeNames.ExpandedPixelExpr.ordinal(), new HashSet<>(Arrays.asList(TIMES, DIV, MOD, PLUS, MINUS, EXP, LT, GT, EQ, LE, GE, BITAND, AND, BITOR, OR, COMMA, RSQUARE, RARROW)));
 	}
 
 	final static ArrayList<HashSet<Kind>> FIRST = new ArrayList<>(ASTNodeNames.values().length);
 
 	static {
-		for (int i = 0; i < ASTNodeNames.values().length; i++) {
-			FIRST.add(new HashSet<Kind>());
-		}
-
-		// FIRST(LogicalOrExpr) = FIRST(LogicalAndExpr) = FIRST(ComparisonExpr) = ...
-		FIRST.get(ASTNodeNames.LogicalOrExpr.ordinal()).addAll(FIRST.get(ASTNodeNames.LogicalAndExpr.ordinal()));
-		FIRST.get(ASTNodeNames.LogicalAndExpr.ordinal()).addAll(FIRST.get(ASTNodeNames.ComparisonExpr.ordinal()));
-		FIRST.get(ASTNodeNames.ComparisonExpr.ordinal()).addAll(FIRST.get(ASTNodeNames.PowExpr.ordinal()));
-		FIRST.get(ASTNodeNames.PowExpr.ordinal()).addAll(FIRST.get(ASTNodeNames.AdditiveExpr.ordinal()));
-		FIRST.get(ASTNodeNames.AdditiveExpr.ordinal()).addAll(FIRST.get(ASTNodeNames.MultiplicativeExpr.ordinal()));
-		FIRST.get(ASTNodeNames.MultiplicativeExpr.ordinal()).addAll(FIRST.get(ASTNodeNames.UnaryExpr.ordinal()));
-
-
-		// FIRST(Expr) = FIRST(ConditionalExpr) + FIRST(LogicalOrExpr)
-		FIRST.get(ASTNodeNames.ConditionalExpr.ordinal()).add(QUESTION);
-
-		FIRST.get(ASTNodeNames.Expr.ordinal()).addAll(FIRST.get(ASTNodeNames.ConditionalExpr.ordinal()));
-		FIRST.get(ASTNodeNames.Expr.ordinal()).addAll(FIRST.get(ASTNodeNames.LogicalOrExpr.ordinal()));
-
-		FIRST.get(ASTNodeNames.UnaryExpr.ordinal()).addAll(Arrays.asList(BANG, MINUS, RES_width, RES_height));
-
-
-		// FIRST(PostfixExpr) = FIRST(PrimaryExpr) = ...
-		FIRST.get(ASTNodeNames.PrimaryExpr.ordinal()).addAll(Arrays.asList(STRING_LIT, NUM_LIT, IDENT, LPAREN, CONST, LSQUARE));
-		FIRST.get(ASTNodeNames.PostfixExpr.ordinal()).addAll(FIRST.get(ASTNodeNames.PrimaryExpr.ordinal()));
-
-		// FIRST(ExpandedPixelExpr) = { [ }
-		FIRST.get(ASTNodeNames.ExpandedPixelExpr.ordinal()).add(LSQUARE);
-
-		// FIRST(ChannelSelector) = { : }
-		FIRST.get(ASTNodeNames.ChannelSelector.ordinal()).add(COLON);
-
-		// FIRST(PixelSelector) = { [ }
-		FIRST.get(ASTNodeNames.PixelSelector.ordinal()).add(LSQUARE);
+		FIRST.set(ASTNodeNames.Expr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height, QUESTION)));
+		FIRST.set(ASTNodeNames.ConditionalExpr.ordinal(), new HashSet<>(Arrays.asList(QUESTION)));
+		FIRST.set(ASTNodeNames.LogicalOrExpr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height)));
+		FIRST.set(ASTNodeNames.LogicalAndExpr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height)));
+		FIRST.set(ASTNodeNames.ComparisonExpr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height)));
+		FIRST.set(ASTNodeNames.PowExpr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height)));
+		FIRST.set(ASTNodeNames.AdditiveExpr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height)));
+		FIRST.set(ASTNodeNames.MultiplicativeExpr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height)));
+		FIRST.set(ASTNodeNames.UnaryExpr.ordinal(), new HashSet<>(Arrays.asList(BANG, MINUS, RES_width, RES_height)));
+		FIRST.set(ASTNodeNames.PostfixExpr.ordinal(), new HashSet<>(Arrays.asList(STRING_LIT, NUM_LIT, BOOLEAN_LIT, IDENT, LPAREN, CONST, LSQUARE)));
+		FIRST.set(ASTNodeNames.PrimaryExpr.ordinal(), new HashSet<>(Arrays.asList(STRING_LIT, NUM_LIT, BOOLEAN_LIT, IDENT, LPAREN, CONST, LSQUARE)));
+		FIRST.set(ASTNodeNames.ChannelSelector.ordinal(), new HashSet<>(Arrays.asList(COLON)));
+		FIRST.set(ASTNodeNames.PixelSelector.ordinal(), new HashSet<>(Arrays.asList( LSQUARE)));
+		FIRST.set(ASTNodeNames.ExpandedPixelExpr.ordinal(), new HashSet<>(Arrays.asList(LSQUARE)));
 	}
 
 
